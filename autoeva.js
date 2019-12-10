236cd430da0162a6507292de0d4aa8cb8be873b1
emeraldzhen <xzz2013@gmail.com>
2019-12-10 14:02:13 +0800

    tampermonkey scirpt

--- autoeva.js
+++ autoeva.js
@@ -1,14 +1,42 @@
-window.setInterval(function(){
-    var j=0;
-    $("table.gridtable a")[j].click();
-    window.setTimeout(function(){
-        for (var i=0,len=6; i<len; i++)
-    {
-        $("input:radio[value=4]")[i].click();
-    }
-    $("input:radio[value=133]")[0].click();
-    $("input#btnSave")[0].click();
-    window.confirm = function(){return !0;};
-    },2000);
-    j++;
-},2000)
+// ==UserScript==
+// @name         AutoEva
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      http://applicationnewjw.ecnu.edu.cn/eams/evaluateStd.action
+// @grant        none
+// @require      http://libs.baidu.com/jquery/1.7.2/jquery.min.js
+// ==/UserScript==
+
+function auto() {
+    var j = 0;
+    var timer = window.setInterval(function () {
+        if ($("table.gridtable a")[j] === undefined) {
+            window.clearInterval(timer);
+        }
+        $("table.gridtable a")[j].click();
+        window.setTimeout(function () {
+            for (var i = 0, len = 6; i < len; i++) {
+                $("input:radio[value=4]")[i].click();
+            }
+            $("input:radio[value=133]")[0].click();
+            var realConfirm = window.confirm;
+            window.confirm = function () {
+                window.confirm = realConfirm;
+                return true;
+            };
+            $("input#btnSave")[0].click();
+        }, 2000);
+        j++;
+    }, 2000)
+}
+
+(function () {
+    var x = document.createElement("button")
+    x.innerHTML = '自动评教'
+    x.id = 'autoeva'
+    document.getElementById('tool_bar').appendChild(x)
+    // Your code here...
+    x.addEventListener("click", auto)
+})();
